8d36d29a0487a2cc0b5974f8b90860f2055c2823
vinay vishwakarma <vnypro98@gmail.com>
2024-12-04 21:31:01 +0530

    added potcard component

--- form-app/src/component/Postcard.jsx
+++ form-app/src/component/Postcard.jsx
@@ -2,31 +2,52 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import styles from "./Postcard.module.css";
 
 const Postcard = () => {
-    const [post, setPost] = useState([]);
-    useEffect(() => {
-        async function fetchPost() {
-            const data = await axios.get("https://dummyjson.com/carts");
-            console.log(data.data);
-            setPost(data.data.carts[0].products)
-        }
-        fetchPost();
-    }, []);
-    return <div>
-        {post.map((item) => {
-            return <div key={item.id}>
-                <img
-                src={item.thumbnail}
-                width={500}
-                height={500}
-                alt="Picture of the author"
-            />
-                <p>{item.title}</p><br /></div>
-        })}
-        {/* <Image src={}/> */}
-    </div>;
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await axios.get("https://dummyjson.com/carts");
+        setPost(response.data.carts[0].products);
+      } catch (error) {
+        setError("Failed to fetch data.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPost();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className={styles.cardContainer}>
+      {post.map((item) => (
+        <div key={item.id} className={styles.card}>
+          <img
+            src={item.thumbnail}
+            alt={item.title}
+            width={300}
+            height={300}
+            className={styles.cardImage}
+          />
+          <h3 className={styles.cardTitle}>{item.title}</h3>
+          <p className={styles.cardPrice}>${item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Postcard;
