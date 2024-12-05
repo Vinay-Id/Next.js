5f2e57b9e1a91443cf43573565c09ec31cb31846
vinay vishwakarma <vnypro98@gmail.com>
2024-12-05 21:10:55 +0530

    made few changes

--- form-app/src/component/Form.jsx
+++ form-app/src/component/Form.jsx
@@ -1,9 +1,9 @@
 "use client"
-
+import styles from './Form.module.css'
 import { useState } from "react";
 
 const Form = () => {
-  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
   const handleChange = (e) => {
     // console.log(e.target);
     const name = e.target.name;
@@ -15,24 +15,33 @@ const Form = () => {
     console.log("Form data submitted:", formData);
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={formData.email}
-          name='email'
-          placeholder="Enter email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          value={formData.password}
-          name='password'
-          placeholder="Enter password"
-          onChange={handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+  <div>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <h1>login</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={formData.name}
+            name='name'
+            placeholder="Enter name"
+            onChange={handleChange} />
+          <input
+            type="email"
+            value={formData.email}
+            name='email'
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            value={formData.password}
+            name='password'
+            placeholder="Enter password"
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
     </div>
   );
 };
