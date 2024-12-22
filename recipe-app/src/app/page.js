2cf09511198c08da02aa7edccb9f43bfb30ded08
vinay vishwakarma <vnypro98@gmail.com>
2024-12-22 21:11:16 +0530

    made changes in recipe app

--- recipe-app/src/app/page.js
+++ recipe-app/src/app/page.js
@@ -1,7 +1,23 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import Recipe from "./[recipe-list]/page";
 
 export default function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipe = () => {
+      fetch("https://dummyjson.com/recipes")
+        .then((res) => res.json())
+        .then((res) => setRecipes(res));
+    };
+    fetchRecipe();
+    console.log(recipes);
+  }, []);
+
   return (
-   <div>recipe</div>
+    <div>
+      <Recipe />
+    </div>
   );
 }
